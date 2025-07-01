44b74a32eb241477519a4da5121f98ec7d410767
Pedro Dias <pedrocorchogdev@gmail.com>
2025-07-01 14:39:37 -0300

    chore: increase decimal float number from 10 to 38

---
+++ Infra/Migrations/20250701165413_IncreasingDecimalPriceInCentsInServiceTable.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreasingDecimalPriceInCentsInServiceTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "price_in_cents",
+                table: "Services",
+                type: "decimal(38,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "price_in_cents",
+                table: "Services",
+                type: "decimal(10,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(38,2)");
+        }
+    }
+}
